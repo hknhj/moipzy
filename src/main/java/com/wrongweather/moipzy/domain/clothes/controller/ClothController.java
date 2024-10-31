fe1accb07c2d7e62141435cc2458b5e0008a5f36
이석주 <nick981213@gmail.com>
2024-10-31 23:26:02 +0900

    ClothController에 주석 추가

--- src/main/java/com/wrongweather/moipzy/domain/clothes/controller/ClothController.java
+++ src/main/java/com/wrongweather/moipzy/domain/clothes/controller/ClothController.java
@@ -24,6 +24,12 @@ public class ClothController {
 
     private final ClothService clothService;
 
+    /**
+     *
+     * @param clothImg
+     * @param clothRegisterRequestDto
+     * @return 옷 정보 json, 옷 사진파일을 받아 db에 저장
+     */
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> uploadCloth(@RequestPart("clothImg") MultipartFile clothImg, @RequestPart("clothData") ClothRegisterRequestDto clothRegisterRequestDto) {
         ClothIdResponseDto clothIdResponseDto = clothService.registerCloth(clothImg, clothRegisterRequestDto);
@@ -31,16 +37,33 @@ public class ClothController {
         return ResponseEntity.status(HttpStatus.CREATED).body("옷 등록 완료. Id : " + clothIdResponseDto.getClothId());
     }
 
+    /**
+     *
+     * @param userId
+     * @param clothId
+     * @return userId, clothId를 이용하여 옷 개별 조회
+     */
     @GetMapping("/{userId}/{clothId}")
     public ClothResponseDto getCloth(@PathVariable("userId") int userId, @PathVariable("clothId") int clothId) {
         return clothService.getCloth(userId, clothId);
     }
 
+    /**
+     *
+     * @param userId
+     * @return 유저가 가진 모든 옷 조회
+     */
     @GetMapping("/{userId}")
     public List<ClothResponseDto> getAllClothes(@PathVariable("userId") int userId) {
         return clothService.getAllClothes(userId);
     }
 
+    /**
+     *
+     * @param userId
+     * @param largeCategory
+     * @return userId와 largeCategory를 사용하여 카테고리별로 옷 리스트를 조회
+     */
     @GetMapping("/largeCategory/{userId}")
     public List<ClothResponseDto> getAllOuter(@PathVariable("userId") int userId, @RequestParam String largeCategory) {
         return clothService.getAllOuter(userId, largeCategory);
