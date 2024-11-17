27cbc41fe0059af3df0c29729b1f09b660b54339
이석주 <nick981213@gmail.com>
2024-11-17 16:37:37 +0900

    - 색상에서 회색 추가 및 아우터 색상 구분할 수 있도록 주석 추가

--- src/main/java/com/wrongweather/moipzy/domain/clothes/category/Color.java
+++ src/main/java/com/wrongweather/moipzy/domain/clothes/category/Color.java
@@ -1,6 +1,8 @@
 package com.wrongweather.moipzy.domain.clothes.category;
 
 public enum Color {
-    BLUE, NAVY, GREEN, BLACK, WHITE, BEIGE, RED, BROWN,      //상의 컬러
-    LIGHTBLUE, DEEPBLUE, KHAKI, CREAM, GREY  //BLACK, BEIGE        //하의 컬러
+    // NAVY, BLACK, BROWN, BEIGE, GREY, RED, BLUE, WHITE
+    BLUE, NAVY, GREEN, BLACK, WHITE, BEIGE, RED, BROWN, GREY,      //상의 컬러
+    LIGHTBLUE, DEEPBLUE, KHAKI, CREAM   //BLACK, BEIGE, GREY        //하의 컬러
+
 }
