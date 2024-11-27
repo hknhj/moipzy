084a273f019917201b01c7bf31f5181f24b9510c
이석주 <nick981213@gmail.com>
2024-11-27 22:40:07 +0900

    - 옷 저장 경로 application.yml과 연동하여 사용하도록 변경

--- src/main/java/com/wrongweather/moipzy/domain/clothImg/service/ClothImgService.java
+++ src/main/java/com/wrongweather/moipzy/domain/clothImg/service/ClothImgService.java
@@ -2,8 +2,8 @@ package com.wrongweather.moipzy.domain.clothImg.service;
 
 import com.wrongweather.moipzy.domain.clothImg.ClothImage;
 import com.wrongweather.moipzy.domain.clothImg.ClothImageRepository;
-import com.wrongweather.moipzy.domain.clothes.Cloth;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,9 +12,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.UUID;
-
-import static com.fasterxml.jackson.databind.jsonFormatVisitors.JsonValueFormat.UUID;
 
 @Service
 @Transactional
@@ -23,13 +20,16 @@ public class ClothImgService {
 
     private final ClothImageRepository clothImageRepository;
 
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
     public ClothImage uploadImage(MultipartFile image) {
         try {
             // 이미지 파일 저장을 위한 경로 설정
-            String uploadsDir = "src/main/resources/static/uploads/clothes/";
+            //String uploadsDir = "src/main/resources/static/uploads/clothes/";
 
             // 이미지 파일 경로를 저장
-            String dbFilePath = saveImage(image, uploadsDir);
+            String dbFilePath = saveImage(image, uploadDir);
 
             // ProductThumbnail 엔티티 생성 및 저장
             ClothImage clothImage = ClothImage.builder()
