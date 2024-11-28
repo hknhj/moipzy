c6afb8628728399f57481921df28b4bd4dbf08d3
이석주 <nick981213@gmail.com>
2024-11-29 01:30:49 +0900

    - kakaoTest controller json 응답 형식 구현

--- src/main/java/com/wrongweather/moipzy/domain/kakao/controller/KakakoController.java
+++ src/main/java/com/wrongweather/moipzy/domain/kakao/controller/KakakoController.java
@@ -3,6 +3,9 @@ package com.wrongweather.moipzy.domain.kakao.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -10,18 +13,33 @@ public class KakakoController {
 
     //카카오톡 오픈빌더로 리턴할 스킬 API
     @PostMapping(value = "/kakaoTest", headers = "Accept=application/json")
-    public String callAPI(@RequestBody Map<String, Object> params) {
+    public HashMap<String, Object> callAPI(@RequestBody Map<String, Object> params) {
 
-        try {
+        HashMap<String, Object> resultJson = new HashMap<>();
+
+        try{
+
             ObjectMapper mapper = new ObjectMapper();
             String jsonInString = mapper.writeValueAsString(params);
-            int x = 0;
             System.out.println(jsonInString);
 
-            return "정상적으로 작동되었습니다";
-        } catch (Exception e) {
-            System.out.println("에러 발생 " + e);
-            return "에러발생";
+            List<HashMap<String,Object>> outputs = new ArrayList<>();
+            HashMap<String,Object> template = new HashMap<>();
+            HashMap<String, Object> simpleText = new HashMap<>();
+            HashMap<String, Object> text = new HashMap<>();
+
+            text.put("text","코딩32 발화리턴입니다.");
+            simpleText.put("simpleText",text);
+            outputs.add(simpleText);
+
+            template.put("outputs",outputs);
+
+            resultJson.put("version","2.0");
+            resultJson.put("template",template);
+
+        } catch (Exception e){
+            e.printStackTrace();
         }
+        return resultJson;
     }
 }
